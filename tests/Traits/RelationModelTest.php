abed1341e40b3fc0c7fc796545bdfde7aac83a96
tu6ge <772364230@qq.com>
2020-10-17 21:51:16 +0800

    test Trait

--- tests/Traits/RelationModelTest.php
+++ tests/Traits/RelationModelTest.php
@@ -3,7 +3,6 @@
 namespace VoyagerSelectRelation\Tests\Traits;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use VoyagerRelationSelector\Tests\DatabaseTestCase;
 use VoyagerRelationSelector\Traits\RelationModel;
@@ -13,25 +12,25 @@ class RelationModelTest extends DatabaseTestCase
     public function testGetParents()
     {
         DB::table('foo')->insert([
-            'id' => 34,
-            'test_parent_id' => 0,
+            'id'                => 34,
+            'test_parent_id'    => 0,
         ]);
 
-        $this->assertEquals([34], (new FooHasParent)->getParents(34));
+        $this->assertEquals([34], (new FooHasParent())->getParents(34));
 
         DB::table('foo')->insert([
-            'id' => 55,
-            'test_parent_id' => 34,
+            'id'                => 55,
+            'test_parent_id'    => 34,
         ]);
-
-        $this->assertEquals([34, 55], (new FooHasParent)->getParents(55));
+        ()
+        $this->assertEquals([34, 55], (new FooHasParent())->getParents(55));
 
         DB::table('foo')->insert([
-            'id' => 67,
-            'test_parent_id' => 55,
+            'id'                => 67,
+            'test_parent_id'    => 55,
         ]);
 
-        $this->assertEquals([34, 55, 67], (new FooHasParent)->getParents(67));
+        $this->assertEquals([34, 55, 67], (new FooHasParent())->getParents(67));
     }
 }
 
@@ -40,6 +39,6 @@ class FooHasParent extends Model
     use RelationModel;
 
     protected $table = 'foo';
-    
+
     protected $parentKey = 'test_parent_id';
 }
